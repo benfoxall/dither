e343042b12bb9fd0be713d97c26650bf63e9d1fa
Ben Foxall <ben@pusher.com>
2016-11-03 08:23:26 +0000

    colors

--- dither.js
+++ dither.js
@@ -27,13 +27,15 @@ ctx.fillRect(0,0,w,h)
 
 
 function dither(){
+  var colorCache = {}
+
   var image = ctx.getImageData(0,0,w,h)
 
   var oldpixel, newpixel, i, quant_error
 
   for (var y = 0; y < h; y++) {
     for (var x = 0; x < w; x++) {
-      i = (x + (y * w)) * 4
+      i = index(x, y)
 
       oldpixel = image.data[i]
 
@@ -41,9 +43,11 @@ function dither(){
       newpixel = closest(oldpixel)
 
       // pixel[x][y]  := newpixel
-      image.data[i]   = newpixel
-      image.data[i+1] = newpixel
-      image.data[i+2] = newpixel
+      var components = color(newpixel)
+      image.data[i]   = components.r
+      image.data[i+1] = components.g
+      image.data[i+2] = components.b
+
 
       // quant_error  := oldpixel - newpixel
       quant_error = oldpixel - newpixel
@@ -68,10 +72,30 @@ function dither(){
   ctx.putImageData(image,0,0)
 
   function closest(p) {
-    return p > 127 ? 255 : 0
+    return p < 50 ? 0 :
+           p < 100 ? 60 :
+           p < 150 ? 100 :
+           p < 200 ? 160 :
+           255
   }
   function index(x,y) {
     return (x + (y * w)) * 4
+  }
+
+  function color(i) {
+
+    if(colorCache[i]) return colorCache[i]
+
+    console.log("mis", i, d3.interpolateViridis(i/255))
+    var c = d3.interpolateViridis(i/255)
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c)
+
+    return colorCache[i] = {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    }
+
   }
 }
 
